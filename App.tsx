233b1353207d4d3d8d7e2321f75965647ef0e2bd
vitoraugusto17 <vitoraugustoeleuterio1@gmail.com>
2024-03-05 18:15:58 -0300

    Feat : Use State

--- App.tsx
+++ App.tsx
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [quantity, setQuantity] = useState<number>(0);
+  const [price, setPrice] = useState<number>(11.3);
+  const  basePrice = 11.3;
+
+  const removeNumber = () => {
+    setQuantity((prevQuantity: number) => {
+      const newQuantity = prevQuantity - 1;
+      setPrice(basePrice * newQuantity)
+      return newQuantity
+    })
+  }
+
+  const addNumber = () => {
+    setQuantity((prevQuantity: number) => {
+      const newQuantity = prevQuantity + 1;
+      setPrice(basePrice * newQuantity)
+      return newQuantity
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Button title='-' onPress={removeNumber}/>
+        <Text>{quantity.toFixed(2)}</Text>
+        <Button title='+' onPress={addNumber}/>
+        <Text>{price.toFixed(2)}</Text>
+
+      </View>
     </View>
   );
 }
